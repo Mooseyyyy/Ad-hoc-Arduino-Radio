81058dbe5b7cc01be6b3b06188d41e0d25674c39
Brady <65260460+Mooseyyyy@users.noreply.github.com>
2023-11-20 13:29:08 -0700

    Update GROUP12.ino

--- GROUP12.ino
+++ GROUP12.ino
@@ -5,27 +5,28 @@ using namespace ace_routine;
 
 RF24 radio(7, 8);
 const byte packetAddress[6] = "bcast";
-unsigned char packet[32] = {0x50, 0x0c}; 
+unsigned char packet[32] = {0x50, 0x0C}; 
 
 
 // CLASS FOR MANAGAGING ALL NODES
 class NodeManager {
 private:
-    int nodeAddresses[32] = {NULL};   //KEEP NODE ADDRESSES
+    unsigned char nodeAddresses[32][32] = {{NULL}};   //KEEP NODE ADDRESSES
+    // First field is each address, second field in neighbours of that address
     unsigned long latestUpdateTime[32] = {0};
 
 public:
-    void updateNodes(int address) {  //UPDATE NODE ADDRESSES
+    void updateNodes(unsigned char address[32]) {  //UPDATE NODE ADDRESSES
         for (int i = 0; i < 32; i++) {
-            if (address == nodeAddresses[i]) {
+            if (address [1] == nodeAddresses[i][1]) {
                 latestUpdateTime[i] = millis();
             }
         }
     }
 
-    int hasNeighbours(int address) {    //CHECK FOR NEIGHBOURS
+    int hasNeighbours(unsigned char address[32]) {    //CHECK FOR NEIGHBOURS
         for (int i = 0; i < 32; i++) {
-            if (address == nodeAddresses[i]) {
+            if (address[1] == nodeAddresses[i][1]) {
                 return 1;
             }
         }
@@ -33,26 +34,35 @@ public:
     }
 
 
-      void addNeighbours(int address) {   //ADD NEIGHBOURS
+      void addNeighbours(unsigned char address[32]) {   //ADD NEIGHBOURS
         for (int i = 0; i < 32; i++) {
-            if (nodeAddresses[i] == NULL) {
-                nodeAddresses[i] = address;
+            if (nodeAddresses[i][1] == NULL) {
+                nodeAddresses[i][1] = address[1];
                 //Skip type, x, source address, and length fields
-                packet[i+3] = address;
+                packet[i+3] = address[1];
                 latestUpdateTime[i] = millis();
                 //Update the length field
                 packet[2] = sizeof(packet-3);
+                addNeighboursNeighbours(address, i);
                 return;
             }
         }
     }
+
+    void addNeighboursNeighbours(unsigned char address[32], int i) {
+      for(int k = 3; k < 27; k++) {
+        nodeAddresses[i][k] = address[k];
+      }
+      }
 
     void refreshneighbours() {        //REFRESH NEIGHBOURS
         unsigned long currentTime = millis();
         for (int i = 0; i < 32; i++) {
             if (currentTime - latestUpdateTime[i] > 6000) {
                 latestUpdateTime[i] = 0;
-                nodeAddresses[i] = 0;
+                for (int j = 0; j < 32; j++) {
+                  nodeAddresses[i][j] = 0;
+                }
                 packet[i+3] = 0;
             }
         }
@@ -63,11 +73,16 @@ public:
         Serial.print("NEIGHBOURING NODES: ");
         for (int i = 0; i < 32; i++) {
             if (nodeAddresses[i] != NULL) {
-                Serial.print(nodeAddresses[i]);
-                Serial.print(" ");
+                Serial.print(nodeAddresses[i][1]);
+                Serial.print(" - ");
+                for (int j = 0; j < 27; j++) {
+                  Serial.print(nodeAddresses[i][j]);
+                  Serial.print(" ");
+                }
             }
+            Serial.print("\n");
         }
-        Serial.print("\n");
+        Serial.print("Updated Nodes");
     }
 };
 
@@ -91,17 +106,18 @@ public:
     NeighbourListener(NodeManager* noder) {
         nodeManager = noder;
     }
-    char input[33];
+    unsigned char input[33];
     int runCoroutine() override {
         COROUTINE_LOOP() {
             if (radio.available()) {
                 radio.read(input, 32);
                 if ((input[0] >> 4) == 5) {
-                   
                        if (nodeManager->hasNeighbours(input[1])) {  
-                        nodeManager->updateNodes(input[1]);        
+                        nodeManager->updateNodes(input[1]);
+                        //Need to add the neighbours of the incoming address        
                     } else {
-                        nodeManager->addNeighbours(input[1]);
+                        nodeManager->addNeighbours(input);
+                        //Need to add the neighbours of the incoming address  
   
                     }
                 }
